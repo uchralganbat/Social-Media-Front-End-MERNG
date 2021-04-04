ccc5b4582e67fc5a3d0bc95a144620f989f7f8c3
Uchral <uchralganbat0220@gmail.com>
2021-04-04 12:59:19 +0800

    edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000'
+    uri: 'proxy": "https://floating-wildwood-03676.herokuapp.com/'
 });
 
 
